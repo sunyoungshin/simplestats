073b9c4604bb1cc00bf4d741b04179717e2a0fdf
Sunyoung Shin <shin@stat.wisc.edu>
2015-08-27 13:36:21 -0500

    Starting from the scratch

--- test_stats.py
+++ test_stats.py
@@ -1,13 +1,13 @@
 from stats import mean
 
 def test_mean():
-        assert(mean([2,4]) == 3)
-test_mean()
+	assert(mean([2,4]) ==3)
+#test_mean()
 
 def test_float_mean():
-        assert(mean([1,2])==1.5)
-test_float_mean()
+	assert(mean([1,2])==1.5)
+#test_float_mean
 
-def test_len_mean():
-	assert(mean([2,4,6]==4)
-test_len_mean()
+def test_neg_mean():
+	assert(mean([-2,2,4])==1.333)
+#test_neg_mean()
