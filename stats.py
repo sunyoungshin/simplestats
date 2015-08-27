447a5008b7d9d56516f08b95cbdf985e584eac55
Sunyoung Shin <shin@stat.wisc.edu>
2015-08-27 14:13:20 -0500

    first std function and test

--- stats.py
+++ stats.py
@@ -3,4 +3,7 @@ def mean(vals):
 	length=len(vals)
 	return float(total)/float(length)
 
+def std(vals):
+	return 1.0
+
 # print mean([2,4])
--- test_stats.py
+++ test_stats.py
@@ -1,4 +1,4 @@
-from stats import mean
+from stats import mean, std
 from nose.tools import assert_equal, assert_almost_equal
 
 def test_mean():
@@ -12,3 +12,8 @@ def test_float_mean():
 def test_neg_mean():
 	assert_almost_equal(mean([-2,2,4]), 1.333, places=3)
 #test_neg_mean()
+
+def test_std1():
+	obs = std([0.0, 2.0])
+	exp= 1.0
+	assert_equal(obs,exp)
